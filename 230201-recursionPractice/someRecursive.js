f53c34edce8edf4e52c44f36200ab8078862f0e1
GeunYoung Jang <zzbtang@gmail.com>
2023-02-02 23:57:55 +0900

    230201-recursionPractice

---
+++ 230201-recursionPractice/someRecursive.js
@@ -0,0 +1,16 @@
+// ===========================================
+/*
+배열과 콜백을 받는 someRecursive라는 재귀 함수를 작성하세요. 
+콜백에 전달될 때 배열의 단일 값이 true를 반환하면 함수는 true를 반환합니다. 
+그렇지 않으면 false를 반환합니다.
+SAMPLE INPUT / OUTPUT
+const isOdd = val => val % 2 !== 0;
+
+someRecursive([1,2,3,4], isOdd) // true
+someRecursive([4,6,8,9], isOdd) // true
+someRecursive([4,6,8], isOdd) // false
+someRecursive([4,6,8], val => val > 10); // false
+*/
+// ===========================================
+
+function someRecursive(arr, cb) {}
