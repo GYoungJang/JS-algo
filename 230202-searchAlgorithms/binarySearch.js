8aeb0d6c6e3f84f6d6ebfd05e02136af6ce11f35
GeunYoung Jang <zzbtang@gmail.com>
2023-02-02 23:55:41 +0900

    230202-searchAlgorithms

---
+++ 230202-searchAlgorithms/binarySearch.js
@@ -0,0 +1,68 @@
+// ============================
+/*
+정렬된 배열과 값을 받아들이고 값이 존재하는 인덱스를 반환하는 
+binarySearch라는 함수를 작성하세요. 
+그렇지 않으면 -1을 반환합니다.
+이 알고리즘은 linearSearch보다 더 효율적이어야 합니다. 
+binarySearch([1,2,3,4,5],2) // 1
+binarySearch([1,2,3,4,5],3) // 2
+binarySearch([1,2,3,4,5],5) // 4
+binarySearch([1,2,3,4,5],6) // -1
+binarySearch([
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+  40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 10) // 2
+binarySearch([
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+  40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 95) // 16
+binarySearch([
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
+  40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 100) // -1
+*/
+// ============================
+function binarySearch(arr, val) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+    if (arr[middle] === val) return middle;
+    else if (arr[middle] < val) left = middle + 1;
+    else right = middle - 1;
+  }
+  return -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5], 2)); // 1
+// console.log(binarySearch([1, 2, 3, 4, 5], 3)); // 2
+// console.log(binarySearch([1, 2, 3, 4, 5], 5)); // 4
+// console.log(binarySearch([1, 2, 3, 4, 5], 6)); // -1
+// console.log(
+//   binarySearch(
+//     [
+//       5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+//       99,
+//     ],
+//     10
+//   )
+// ); // 2
+// console.log(
+//   binarySearch(
+//     [
+//       5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+//       99,
+//     ],
+//     95
+//   )
+// ); // 16
+// console.log(
+//   binarySearch(
+//     [
+//       5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+//       99,
+//     ],
+//     100
+//   )
+// ); // -1
---
+++ 230202-searchAlgorithms/linearSearch.js
@@ -0,0 +1,29 @@
+// =========================================
+/*
+배열과 값을 받아들이고 값이 존재하는 인덱스를 반환하는 
+linearSearch라는 함수를 작성하세요. 
+값이 배열에 없으면 -1을 반환합니다.
+이 기능을 구현하기 위해 indexOf를 사용하지 마십시오!
+linearSearch([10, 15, 20, 25, 30], 15) // 1
+linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4) // 5
+linearSearch([100], 100) // 0
+linearSearch([1,2,3,4,5], 6) // -1
+linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 10) // -1
+linearSearch([100], 200) // -1
+*/
+// =========================================
+
+function linearSearch(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) {
+      return i;
+    }
+  }
+  return -1;
+}
+console.log(linearSearch([10, 15, 20, 25, 30], 15)); // 1
+console.log(linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4)); // 5
+console.log(linearSearch([100], 100)); // 0
+console.log(linearSearch([1, 2, 3, 4, 5], 6)); // -1
+console.log(linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 10)); // -1
+console.log(linearSearch([100], 200)); // -1
