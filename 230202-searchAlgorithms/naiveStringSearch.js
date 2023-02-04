a2d55e6157ead00ae111bc8bb59a61a1a0df223e
GeunYoung Jang <zzbtang@gmail.com>
2023-02-04 18:14:44 +0900

    230203-searchAlgorithms

--- 230202-searchAlgorithms/naiveStringSearch.js
+++ 230202-searchAlgorithms/naiveStringSearch.js
@@ -1,12 +1,10 @@
 function naiveStringSearch(str1, str2) {
-  let str1Length = str1.length;
-  let str2Length = str2.length;
   let str1Index = 0;
   let count = 0;
 
-  while (str1Index <= str1Length - str2Length) {
+  while (str1Index <= str1.length - str2.length) {
     let str2Index = 0;
-    while (str2Index < str2Length) {
+    while (str2Index < str2.length) {
       if (str1[str1Index] === str2[str2Index]) {
         str1Index++;
         str2Index++;
@@ -14,10 +12,22 @@ function naiveStringSearch(str1, str2) {
         str1Index++;
         break;
       }
+      if (str2Index === str2.length) count++;
     }
-    count++;
   }
   return count;
 }
 
-console.log(naiveStringSearch('lorie loled', 'lol'));
+console.log(naiveStringSearch('lorie loled', 'e'));
+
+// 강의
+function naiveSearch(long, short) {
+  var count = 0;
+  for (var i = 0; i < long.length; i++) {
+    for (var j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+}
