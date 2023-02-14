6d6d58b7e6e27954903b32652ffac87429b7f455
GeunYoung Jang <zzbtang@gmail.com>
2023-02-14 21:41:58 +0900

    230214-fix

--- 230127-patternsPractice/FrequencyCounter/MultiplePointers-areThereDuplicates.js
+++ 230127-patternsPractice/FrequencyCounter/MultiplePointers-areThereDuplicates.js
@@ -17,8 +17,10 @@ Space - O(1)
 
 // 나
 function areThereDuplicates(...arg) {
+  console.log(arg);
   if (arg.length <= 1) return false;
   let sortedArg = arg.sort((a, b) => a - b);
+  console.log(sortedArg);
   for (let i = 0; i < sortedArg.length - 1; i++) {
     if (sortedArg[i] === sortedArg[i + 1]) {
       return true;
@@ -27,40 +29,40 @@ function areThereDuplicates(...arg) {
   return false;
 }
 
-console.log(areThereDuplicates(1, 2, 3));
-console.log(areThereDuplicates(1, 2, 2));
-console.log(areThereDuplicates('a', 'b', 'c', 'a'));
+console.log(areThereDuplicates(3, 2, 1));
+// console.log(areThereDuplicates(1, 2, 2));
+// console.log(areThereDuplicates('a', 'b', 'c', 'a'));
 
 // 강의
 // 빈도수 세기
-function areThereDuplicates() {
-  let collection = {};
-  for (let val in arguments) {
-    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1;
-  }
-  for (let key in collection) {
-    if (collection[key] > 1) return true;
-  }
-  return false;
-}
+// function areThereDuplicates() {
+//   let collection = {};
+//   for (let val in arguments) {
+//     collection[arguments[val]] = (collection[arguments[val]] || 0) + 1;
+//   }
+//   for (let key in collection) {
+//     if (collection[key] > 1) return true;
+//   }
+//   return false;
+// }
 
-// 다중 포인터
-function areThereDuplicates(...args) {
-  // Two pointers
-  args.sort((a, b) => a > b);
-  let start = 0;
-  let next = 1;
-  while (next < args.length) {
-    if (args[start] === args[next]) {
-      return true;
-    }
-    start++;
-    next++;
-  }
-  return false;
-}
+// // 다중 포인터
+// function areThereDuplicates(...args) {
+//   // Two pointers
+//   args.sort((a, b) => a > b);
+//   let start = 0;
+//   let next = 1;
+//   while (next < args.length) {
+//     if (args[start] === args[next]) {
+//       return true;
+//     }
+//     start++;
+//     next++;
+//   }
+//   return false;
+// }
 
-// 한 줄 풀이
-function areThereDuplicates() {
-  return new Set(arguments).size !== arguments.length;
-}
+// // 한 줄 풀이
+// function areThereDuplicates() {
+//   return new Set(arguments).size !== arguments.length;
+// }
